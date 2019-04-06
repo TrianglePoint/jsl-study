af590a8f338c409d2c26802c47d264ce1408f98b
DESKTOP-6JRESFM <rlarbgus10@gmail.com>
2019-04-06 23:18:39 +0900

    Update to JQuery version
     > html
        - add the script tag : jquery (cdn: googleapis)
        - delete the onload attribute of body
     > js
        - getElementById("id") to $("#id")
        - value to val()
        - innerHTML to html()
        - remove as loop to empty()

--- RegistHTML/WebContent/regist.js
+++ RegistHTML/WebContent/regist.js
@@ -1,22 +1,21 @@
 
 // 페이지 로드시 호출.
-function firstRun(){
+$(document).ready(function(){
 	let lastMonth = 12; // 1년은 12월까지.
 	
-	let select_month = document.getElementById("select_month");
 	for(let i = 1; i <= lastMonth; i++){
-		let month = document.createElement("option"); // 12월까지 추가하고.
-		month.value= i;
-		month.innerHTML = i;
-		select_month.appendChild(month);
+		let month = $("<option>"); // 12월까지 추가하고.
+		month.val(i);
+		month.html(i);
+		$("#select_month").append(month);
 	}
 	
 	setMaxDay(1); // 1월이 기본값이니 1월의 날들을 추가하는 함수 호출.
-}
+});
 
 // 현재 선택된 달을 확인.
 function checkMonth(){
-	let month = document.getElementById("select_month").value;
+	let month = $("#select_month").val();
 	if(month == 2){
 		setMaxDay(month); // 2월이면 윤년인지 확인해야함.
 	}
@@ -25,44 +24,41 @@ function checkMonth(){
 // 이달의 day들을 설정함.
 function setMaxDay(month){
 
-	let select_day = document.getElementById("select_day");
-	let previousDay = select_day.value; // 사용자가 기존에 날짜를 선택했을 경우를 대비해 날짜 저장.
+	let select_day = $("#select_day");
 	
+	let previousDay = select_day.val(); // 사용자가 기존에 날짜를 선택했을 경우를 대비해 날짜 저장.
+
 	removeDay(); // day들을 지움
 	
 	let maxDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 달마다의 마지막 날짜.
 	
 	if(month == 2){ // 윤년 계산해야함.
-		let year = document.getElementById("input_year").value;
-		if(isLeapYear(year)){
+		if(isLeapYear( $("#input_year").val() )){
 			// 윤년이다.
 			maxDay[1] = 29;
 		}
 	}
 
 	for(let i = 1; i <= maxDay[month-1]; i++){
-		let day = document.createElement("option"); // Day 추가.
-		day.value= i;
-		day.innerHTML = i;
-		select_day.appendChild(day);
+		let day = $("<option>"); // Day 추가.
+		day.val(i);
+		day.html(i);
+		select_day.append(day);
 	}
 	
 	// 사용자가 선택한 이전 day가 있는가?
-	if(previousDay != ""){
+	if(previousDay != null){
 		if(previousDay <= maxDay[month-1]){
-			select_day.value = previousDay; // 사용자가 선택한 이전 day로 복구.  
+			select_day.val(previousDay); // 사용자가 선택한 이전 day로 복구.  
 		}else{
-			select_day.value = maxDay[month-1]; // 예) 이전 day가 31일인데, 1월일 경우 30일로 설정.
+			select_day.val(maxDay[month-1]); // 예) 이전 day가 31일인데, 1월일 경우 30일로 설정.
 		}
 	} 
 }
 
 // day 지움.
 function removeDay(){
-	let select_day = document.getElementById("select_day");
-	while(select_day.firstChild){
-		select_day.removeChild(select_day.firstChild); // 첫번째 child가 없다고 할때까지 첫번째 child 제거.
-	}
+	$("#select_day").empty();
 }
 
 // 윤년인가?
@@ -105,13 +101,13 @@ function validate(){
 
 // 아이디 검사.
 function validateId(){
-	let input_id = document.getElementById("input_id");
+	let input_id = $("#input_id");
 	let regExp = /^[A-Za-z0-9]{4,12}$/; // 4~12자의 영문,숫자인가?
-	let result = regExp.test(input_id.value);
+	let result = regExp.test(input_id.val());
 	
 	if(!(result)){
 		window.alert("아이디: 4~12자의 영문 대소문자와 숫자로만 입력해야합니다");
-		input_id.value = "";
+		input_id.val("");
 		input_id.focus();
 		return false;
 	}
@@ -121,31 +117,31 @@ function validateId(){
 
 // 비밀번호 검사.
 function validatePwd(){
-	let input_id = document.getElementById("input_id");
-	let input_pwd = document.getElementById("input_pwd");
+	let input_id = $("#input_id");
+	let input_pwd = $("#input_pwd");
 	
-	if(input_id.value == input_pwd.value){ // 아이디, 비밀번호는 달라야한다.
+	if(input_id.val() == input_pwd.val()){ // 아이디, 비밀번호는 달라야한다.
 		window.alert("아이디와 비밀번호는 달라야합니다");
-		input_pwd.value = "";
+		input_pwd.val("");
 		input_pwd.focus();
 		return false;
 	}
 	
 	let regExp = /^[A-Za-z0-9]{4,12}$/; // 4~12자의 영문, 숫자인가?
-	let result = regExp.test(input_pwd.value);
+	let result = regExp.test(input_pwd.val());
 	
 	if(!(result)){
 		window.alert("비밀번호: 4~12자의 영문 대소문자와 숫자로만 입력해야합니다");
-		input_pwd.value = "";
+		input_pwd.val("");
 		input_pwd.focus();
 		return false;
 	}
 	
-	let input_pwdConfirm = document.getElementById("input_pwdConfirm");
+	let input_pwdConfirm = $("#input_pwdConfirm");
 	
-	if(input_pwd.value != input_pwdConfirm.value){ // 비밀번호 확인란은 비밀번호랑 같아야한다.
+	if(input_pwd.val() != input_pwdConfirm.val()){ // 비밀번호 확인란은 비밀번호랑 같아야한다.
 		window.alert("비밀번호 확인: 비밀번호 확인란을 비밀번호란과 똑같이 입력해야합니다");
-		input_pwdConfirm.value = "";
+		input_pwdConfirm.val("");
 		input_pwdConfirm.focus();
 		return false;
 	}
@@ -155,13 +151,13 @@ function validatePwd(){
 
 // 메일주소 검사.
 function validateMail(){
-	let input_mail = document.getElementById("input_mail");
+	let input_mail = $("#input_mail");
 	let regExp = /^\w{1,}\@\w{1,}\.(com|net|co\...|org|go\...|ac\...)$/; // asd@asd.com, co.kr 같은 양식이여야한다.
-	let result = regExp.test(input_mail.value);
+	let result = regExp.test(input_mail.val());
 	
 	if(!(result)){
 		window.alert("메일주소: 양식에 맞춰야합니다");
-		input_mail.value = "";
+		input_mail.val("");
 		input_mail.focus();
 		return false;
 	}
@@ -171,12 +167,12 @@ function validateMail(){
 
 // 이름 검사.
 function validateName(){
-	let input_name = document.getElementById("input_name");
+	let input_name = $("#input_name");
 	let regExp = /^\S{1,}$/; // 공백은 안됨.
-	let result = regExp.test(input_name.value);
+	let result = regExp.test(input_name.val());
 	if(!(result)){
 		window.alert("이름: 공백없이 이름을 쓰세요");
-		input_name.value = "";
+		input_name.val("");
 		input_name.focus();
 		return false;
 	}
@@ -207,19 +203,19 @@ function checkJoomin(joomin){
 
 // 생년월일의 생년을 설정.
 function setInput_year(year){
-	document.getElementById("input_year").value = year;
+	$("#input_year").val(year);
 }
 
 // 주민번호 검사.
 function validateJoomin(){
-	let input_joomin = document.getElementById("input_joomin");
+	let input_joomin = $("#input_joomin");
 	let regExp = /^\d{13}$/; // 주민번호는 13자리의 숫자다.
-	let joomin = input_joomin.value;
+	let joomin = input_joomin.val();
 	let result = regExp.test(joomin);
 	
 	if(!(result)){
 		window.alert("주민번호: 13자리이여야합니다");
-		input_joomin.value = "";
+		input_joomin.val("");
 		input_joomin.focus();
 		return false;
 	}
@@ -235,7 +231,7 @@ function validateJoomin(){
 	// 계산을 거친 결과가 주민번호 맨 뒷자리와 동일해야한다.
 	if((11 - (sum%11))%10 != joomin[joomin.length-1]){
 		window.alert("주민번호: 정상적인 주민번호를 입력해야합니다");
-		input_joomin.value = "";
+		input_joomin.val("");
 		input_joomin.focus();
 		return false;
 	}
